616a6cfff69f39265819d98eefd24ed4bb9ed3b2
ShayanJamil519 <shayanjamil500@gmail.com>
2023-09-24 14:44:15 +0500

    Home Page Responsiveness Done ✅

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <div className="w-[80%] mx-auto mt-20 text-[#fff] text-center ">
-      <h1 className="text-5xl  uppercase font-normal  tracking-normal">
+    <div className="w-[90%] sm:w-[80%] mx-auto mt-20 text-[#fff] text-center ">
+      <h1 className="text-2xl sm:text-4xl lg:text-5xl  uppercase font-normal  tracking-normal">
         Why Fear When{" "}
       </h1>
-      <h1 className="text-[80px] mb-4 uppercase font-medium tracking-wide">
+      <h1 className=" text-[70px]    lg:text-[80px] mb-4 uppercase font-medium  sm:tracking-wide">
         We Are Here !
       </h1>
       <p>
--- src/components/home/NewsLetter.jsx
+++ src/components/home/NewsLetter.jsx
@@ -5,29 +5,29 @@ const NewsLetter = () => {
     <div className="my-10">
       {/* Heading */}
       <div className="mb-10">
-        <h1 className="text-5xl font-bold text-center text-[#fff] uppercase">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center text-[#fff] uppercase">
           New-Letter
         </h1>
-        <hr className="bg-[#fff] w-40 mx-auto mt-2 " />
+        <hr className="bg-[#fff] w-20 md:w-40 mx-auto mt-2 " />
       </div>
       {/* Content */}
       <div>
-        <h1 className="text-xl font-medium text-center text-[#fff] uppercase">
+        <h1 className="text-lg md:text-xl font-medium text-center text-[#fff] uppercase">
           Subscribe to our news letter
         </h1>
-        <p className="text-sm font-light text-center text-[#fff] uppercase">
+        <p className="text-xs sm:text-sm font-light text-center text-[#fff] uppercase">
           To receive news abut upcoming products, best sellers, sales
         </p>
 
         {/* Email */}
-        <form className="text-center mt-5">
+        <form className=" flex gap-4 flex-col sm:flex-row justify-center items-center w-[80%] sm:w-[60%]  lg:w-[35%] mx-auto mt-5">
           <input
             type="email"
             required
             placeholder="Please enter your email"
-            className="w-1/4 px-3 py-2 focus:border-slate-500 font-base focus:border-2 border-none rounded-md focus:outline-none"
+            className="w-full px-3 py-2 focus:border-slate-500 font-base focus:border-2 border-none rounded-md focus:outline-none"
           />
-          <button className="bg-[#000] border-[1px] uppercase border-[#fff] text-[#fff] py-2 px-3 rounded-lg ml-2 hover:bg-[#290e0e77]  ">
+          <button className="bg-[#000] w-full sm:w-[35%] border-[1px] uppercase border-[#fff] text-[#fff] py-2 px-3 rounded-lg  hover:bg-[#290e0e77]  ">
             Subscribe
           </button>
         </form>
